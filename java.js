14b5f4edd6f114823aefd3736e7982d8298fa121
SamuelZanon14 <samuelzanon14@gmail.com>
2024-09-06 11:24:51 -0300

    001

---
+++ java.js
@@ -0,0 +1,13 @@
+// script.js
+function changeNameAndBackground() {
+    // Mudar o texto do título
+    document.getElementById('name').innerText = 'Oi, sou Jorge Pata Mansa';
+
+    // Mudar o texto do poster
+    document.getElementById('poster-name').innerText = 'Nome: Jorge Pata Mansa';
+
+    // Mudar a cor de fundo da página
+    document.body.style.backgroundColor = '#d3d3d3'; // Cinza claro
+}
+
+
